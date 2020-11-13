0f10c38044218b5639aac328532a1ccf41d80688
Guilherme Siqueira <guilhersiqueira@gmail.com>
2020-11-12 22:01:37 -0300

    fixing closest pair distance

--- app/closest_pair_of_points.js
+++ app/closest_pair_of_points.js
@@ -6,91 +6,106 @@ class Point {
   }
 }
 
-let point;
+var mergeSort = function mergeSort(points, comp) {
+	if(points.length < 2) return points;
+ 
+ 
+	var n = points.length,
+		i = 0,
+		j = 0,
+		leftN = Math.floor(n / 2),
+		rightN = leftN;
+ 
+ 
+	var leftPart = mergeSort( points.slice(0, leftN), comp),
+		rightPart = mergeSort( points.slice(rightN), comp );
+ 
+	var sortedPart = [];
+ 
+	while((i < leftPart.length) && (j < rightPart.length)) {
+		if(comp(leftPart[i], rightPart[j]) < 0) {
+			sortedPart.push(leftPart[i]);
+			i += 1;
+		}
+		else {
+			sortedPart.push(rightPart[j]);
+			j += 1;
+		}
+	}
+	while(i < leftPart.length) {
+		sortedPart.push(leftPart[i]);
+		i += 1;
+	}
+	while(j < rightPart.length) {
+		sortedPart.push(rightPart[j]);
+		j += 1;
+	}
+	return sortedPart;
+};
+ 
+var closestPair = function _closestPair(Px, Py) {
+	if(Px.length < 2) return { distance: Infinity, pair: [ new Point(0, 0), new Point(0, 0) ] };
+	if(Px.length < 3) {
+		//find euclid distance
+		var d = Math.sqrt( Math.pow(Math.abs(Px[1].x - Px[0].x), 2) + Math.pow(Math.abs(Px[1].y - Px[0].y), 2) );
+		return {
+			distance: d,
+			pair: [ Px[0], Px[1] ]
+		};
+	}
+ 
+	var	n = Px.length,
+		leftN = Math.floor(n / 2),
+		rightN = leftN;
+ 
+	var Xl = Px.slice(0, leftN),
+		Xr = Px.slice(rightN),
+		Xm = Xl[leftN - 1],
+		Yl = [],
+		Yr = [];
+	//separate Py
+	for(var i = 0; i < Py.length; i += 1) {
+		if(Py[i].x <= Xm.x)
+			Yl.push(Py[i]);
+		else
+			Yr.push(Py[i]);
+	}
+ 
+	var dLeft = _closestPair(Xl, Yl),
+		dRight = _closestPair(Xr, Yr);
+ 
+	var minDelta = dLeft.distance,
+		closestPair = dLeft.pair;
+	if(dLeft.distance > dRight.distance) {
+		minDelta = dRight.distance;
+		closestPair = dRight.pair;
+	}
+ 
+ 
+	//filter points around Xm within delta (minDelta)
+	var closeY = [];
+	for(i = 0; i < Py.length; i += 1) {
+		if(Math.abs(Py[i].x - Xm.x) < minDelta) closeY.push(Py[i]);
+	}
+	//find min within delta. 8 steps max
+	for(i = 0; i < closeY.length; i += 1) {
+		for(var j = i + 1; j < Math.min( (i + 8), closeY.length ); j += 1) {
+			var d = Math.sqrt( Math.pow(Math.abs(closeY[j].x - closeY[i].x), 2) + Math.pow(Math.abs(closeY[j].y - closeY[i].y), 2) );
+			if(d < minDelta) {
+				minDelta = d;
+				closestPair = [ closeY[i], closeY[j] ]
+			}
+		}
+	}
+ 
+	return {
+		distance: minDelta,
+		pair: closestPair
+	};
+};
 
-// Find distance between two points
-const dist = (p1, p2) => {
-  return Math.sqrt(
-    (p1.x - p2.x) *
-    (p1.x - p2.x) +
-    (p1.y - p2.y) *
-    (p1.y - p2.y)
-  );
-}
-
-// A Brute Force method to return the
-// smallest distance between two points
-const bruteForce = (points) => {
-  let minVal = Infinity;
-  for (let i = 0; i < points.length; i++) {
-    for (let j = i + 1; j < points.length; j++) {
-      if (dist(points[i], points[j]) < minVal) {
-        minVal = dist(points[i], points[j]);
-        point = { origin: points[i], destionation: points[j] };
-      }
-    }
-  }
-  return minVal;
-}
-
-// A utility function to find the
-// distance beween the closest points of
-// strip of given size. All points in
-// strip[] are sorted accordint to 
-// y coordinate. They all have an upper
-// bound on minimum distance as d.
-// Note that this method seems to be
-// a O(n^2) method, but it's a O(n)
-// method as the inner loop runs at most 6 times
-const stripClosest = (strip, d) => {
-  let minVal = d;
-
-  for (let i = 0; i < strip.length; i++) {
-    let j = i + 1;
-    while (j < strip.length && ((strip[j].y - strip[i].y) < minVal)) {
-      minVal = dist(strip[i], strip[j]);
-      point = { origin: strip[i], destionation: strip[j] };
-      j += 1;
-    }
-  }
-
-  return minVal;
-}
-
-// A recursive function to find the
-// smallest distance. The array P contains
-// all points sorted according to x coordinate
-const closestUtil = (P, Q, n) => {
-  if (n <= 3) {
-    return bruteForce(P);
-  }
-
-  let mid = Math.floor(n / 2);
-  let midPoint = P[mid];
-
-  let dl = closestUtil(P.slice(0, mid), Q, mid);
-  let dr = closestUtil(P.slice(mid, P.length), Q, n - mid);
-
-  let d = Math.min(dl, dr);
-
-  let strip = [];
-  for (let i = 0; i < n; i++) {
-    if (Math.abs(Q[i].x - midPoint.x) < d) {
-      strip.push(Q[i]);
-    }
-  }
-
-  return Math.min(d, stripClosest(strip, d));
-}
-
-// The main function that finds the smallest distance
-// This method mainly uses closestUtil()
-const closest = (points) => {
-  let P = points.sort((a, b) => a.x - b.x);
-  let Q = points.sort((a, b) => a.y - b.y);
-
-  return closestUtil(P, Q, points.length);
-}
+var sortX = function (a, b) { return (a.x < b.x) ? -1 : ((a.x > b.x) ? 1 : 0); }
+var sortY = function (a, b) { return (a.y < b.y) ? -1 : ((a.y > b.y) ? 1 : 0); }
 
 const CANVAS_SIZE = 500;
 const POINT_SIZE = 2;
@@ -104,29 +119,29 @@ const getRandomPoint = () => {
   return new Point(getRandomInt(CANVAS_SIZE), getRandomInt(CANVAS_SIZE));
 }
 
-const draw = (x, y, canvas) => {
+const draw = (x, y, canvas = document.getElementById('canvas').getContext('2d')) => {
   canvas.beginPath();
   canvas.arc(x, y, POINT_SIZE, 0, Math.PI * 2, true);
   canvas.fill();
 }
 
-const line = (O, D, canvas) => {
+const line = (O, D, canvas = document.getElementById('canvas').getContext('2d')) => {
   canvas.moveTo(O.x, O.y);
   canvas.lineTo(D.x, D.y);
   canvas.stroke();
 }
 
 window.onload = () => {
-  const canvas = document.getElementById('canvas').getContext('2d');
-
   const points = [...new Array(POINTS_QUANTITY)].map(() => getRandomPoint());
 
   points.forEach(point => {
-    draw(point.x, point.y, canvas);
+    draw(point.x, point.y);
   });
 
-  const distance = closest(points);
-  console.log(distance);
+  var Px = mergeSort(points, sortX);
+  var Py = mergeSort(points, sortY);
 
-  line(point.origin, point.destionation, canvas);
+  const { pair } = closestPair(Px, Py);
+
+  line(pair[0], pair[1]);
 }
